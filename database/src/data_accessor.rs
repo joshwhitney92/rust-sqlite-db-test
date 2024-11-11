0eafb99f8d4a07389b4ebdbc98fbd71f3320064a
josh <joshwhtny@yahoo.com>
2024-11-11 12:33:58 -0800

    working repository pattern - first try

--- database/src/data_accessor.rs
+++ database/src/data_accessor.rs
@@ -1,25 +1,67 @@
+use crate::errors::Error as DBError;
+use crate::models::{QueryResult, RemoteJob};
+use sqlx::sqlite::SqliteQueryResult;
 use sqlx::Error;
 use tokio::runtime::Runtime;
 
-use crate::models::RemoteJob;
+/*
+ * TODO: Try and make ths work!
+pub async fn exec_query_async<T: QueryResult>(
+    connection_string: &str,
+    query: &str,
+    result: &mut dyn QueryResult
+) -> Result<T, DBError> {
+    let pool = sqlx::sqlite::SqlitePool::connect("sqlite:test.db").await;
+    match pool {
+        Ok(connection_pool) => {
+            let _: Vec<&QueryResult> =
+            sqlx::query_as!(T, "SELECT * FROM RemoteJobs")
+                .fetch_all(&pool)
+                .await?;
+            // let query: SqliteQueryResult = sqlx::query(query)
+            //     .execute(&connection_pool)
+            //     .await
+            //     .unwrap();
+
+            //  let parsed: T = query.try_into();
+
+            // let result = sqlx::query_as_with(sql, arguments)
+            //     .fetch_all(connection_pool)
+            //     .await;
+
+            Ok(())
+        }
+        Err(_) => Err(DBError::DatabaseError {
+            message: String::from("Error connecting to database"),
+            code: 1,
+        }),
+    }
+}
+*/
 
 pub async fn get_remote_jobs() -> Result<Vec<RemoteJob>, Error> {
     let pool = sqlx::sqlite::SqlitePool::connect("sqlite:test.db").await?;
-    let remote_jobs: Vec<RemoteJob> = sqlx::query_as!(RemoteJob, "SELECT * FROM RemoteJobs")
+    let remote_jobs: Vec<RemoteJob> = sqlx::query_as!(RemoteJob, "
+            SELECT 
+             PKRemoteJobs as RemoteJobID,
+             Name,
+             Url,
+             FKCategory as Category
+            FROM RemoteJobs
+
+        ")
         .fetch_all(&pool)
         .await?;
 
     Ok(remote_jobs)
 }
 
-
 pub fn get_remote_jobs_sync() -> Result<Vec<RemoteJob>, Error> {
-
     let rt = tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()
         .unwrap();
 
     // Execute the future, blocking the current thread until completion
-    rt.block_on(async {get_remote_jobs().await })
+    rt.block_on(async { get_remote_jobs().await })
 }
--- driver/src/main.rs
+++ driver/src/main.rs
@@ -2,13 +2,24 @@ use std::env::join_paths;
 
 use database::*;
 use models::RemoteJob;
+use repositories::remote_jobs::{RemoteJobRepository, TRemoteJobRepository};
 const DB_PATH: &str = "test.db";
 
 fn main() {
     if let Ok(db_file) = file_manager::create_file(DB_PATH, false) {}
 
-    if let Ok(jobs) = database::data_accessor::get_remote_jobs_sync() {
-        jobs.iter().for_each(|job| println!("{:?}", job));
-    }
+    let repo = RemoteJobRepository::new();
 
+    // if let Ok(jobs) = database::data_accessor::get_remote_jobs_sync() {
+    match repo.GetRemoteJobs() {
+        Ok(jobs) => {
+            jobs.iter().for_each(|job| println!("{:?}", job));
+        },
+        Err(x) => {
+            println!("{:?}", x.to_string())
+        }
+    }
+    // if let Ok(jobs) = repo.GetRemoteJobs() {
+    //     jobs.iter().for_each(|job| println!("{:?}", job));
+    // }
 }
